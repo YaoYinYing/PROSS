a3dac5fc30e35400bb5d11457d44649b2a50849b
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-09-24 17:10:22 +0800

    fix:script:deps

--- tests/scripts/setup_for_ci.sh
+++ tests/scripts/setup_for_ci.sh
@@ -1,6 +1,7 @@
 pip install rosetta_finder -U
-apt-get install perl wget -y
+apt-get update -y; apt-get install perl wget -y
 
 wget https://ftpmirror.gnu.org/parallel/parallel-latest.tar.bz2 -O parallel.tar.bz2
+
 tar -xf parallel.tar.bz2; rm -f parallel.tar.bz2;
 cd $(ls |grep '^parallel-20' |head -1); ./configure && make && make install
